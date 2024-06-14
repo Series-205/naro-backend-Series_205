536f57c036660526657ac645d9375113813d57e4
Series-205 <esio.trot28@gmail.com>
2024-06-14 18:13:57 +0900

    copy (server-database)

--- main.go
+++ main.go
@@ -3,24 +3,29 @@ package main
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
-// #region city
 type City struct {
-	ID          int    `json:"ID,omitempty" db:"ID"`
-	Name        string `json:"name,omitempty" db:"Name"`
+	ID          int    `json:"id,omitempty"  db:"ID"`
+	Name        string `json:"name,omitempty"  db:"Name"`
 	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
 	District    string `json:"district,omitempty"  db:"District"`
 	Population  int    `json:"population,omitempty"  db:"Population"`
 }
 
-// #endregion city
+var (
+	db *sqlx.DB
+)
+
 func main() {
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
@@ -38,47 +43,34 @@ func main() {
 		Loc:       jst,
 	}
 
-	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	_db, err := sqlx.Open("mysql", conf.FormatDSN())
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	log.Println("connected")
-	// #region get
+	db = _db
 
-	cityName := "Tokyo"
-	if len(os.Args) >= 2 {
-		cityName = os.Args[1]
-	}
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityInfoHandler)
+
+	e.Start(":8080")
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	log.Println(cityName)
 
 	var city City
-	err = db.Get(&city, "SELECT * FROM city WHERE Name = ?", cityName)
+	err := db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
 	if errors.Is(err, sql.ErrNoRows) {
-		log.Printf("no such city Name = '%s'\n", cityName)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("No such city Name = %s", cityName))
 	}
 	if err != nil {
-		log.Fatalf("DB Error: %s\n", err)
-	}
-	// #endregion get
-	log.Printf("%sの人口は%d人です\n", cityName, city.Population)
-
-	var countryPop int
-	err = db.Get(&countryPop, "SELECT Population FROM country WHERE Code = ?", city.CountryCode)
-	if err != nil {
-		log.Fatalf("DB Error: %s\n", err)
-	}
-	log.Printf("%sの人口は%sの人口の%f%%です\n", city.Name, city.CountryCode, float64(city.Population)/float64(countryPop)*100)
-
-	var cities []City
-	err = db.Select(&cities, "SELECT * FROM city WHERE CountryCode = 'JPN'") //?を使わない場合、第3引数以降は不要
-	if err != nil {
-		log.Fatal(err)
+		log.Printf("DB Error: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
 	}
 
-	log.Println("日本の都市一覧")
-	for _, city := range cities {
-		log.Printf("都市名: %s, 人口: %d\n", city.Name, city.Population)
-	}
+	return c.JSON(http.StatusOK, city)
 }
